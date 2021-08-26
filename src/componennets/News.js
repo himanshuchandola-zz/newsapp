40417e161dd925eccf4b501e5132a02ede00dc97
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 18:39:28 +0530

    changes in news.js and newsitem.js

--- src/componennets/News.js
+++ src/componennets/News.js
@@ -2,20 +2,65 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+    articles = [
+        {
+            "source": {
+                "id": "abc-news-au",
+                "name": "ABC News (AU)"
+            },
+            "author": "ABC News",
+            "title": "England cricket great Ted Dexter dies aged 86",
+            "description": "The former England captain, nicknamed \"Lord Ted\", is fondly remembered as a giant of the game and one of his country's greatest players.",
+            "url": "http://www.abc.net.au/news/2021-08-26/england-cricket-great-ted-dexter-dies-aged-86/100411276",
+            "urlToImage": "https://live-production.wcms.abc-cdn.net.au/1006c7ecf34ec0935eef2aade5017711?impolicy=wcms_crop_resize&cropH=2815&cropW=5004&xPos=0&yPos=223&width=862&height=485",
+            "publishedAt": "2021-08-26T09:07:52Z",
+            "content": "Former England captain Ted Dexter has died aged 86 following a recent illness.\r\n<ul><li>Dexter played 62 Tests for England and was captain on 30 occasions</li><li>He was inducted into the ICC Hall of… [+1746 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+            "publishedAt": "2020-04-27T11:41:47Z",
+            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+            "publishedAt": "2020-03-30T15:26:05Z",
+            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+        }
+    ]
+    constructor(){
+        super();
+        console.log("Hello i am himanshu");
+        this.state = {
+            articles: this.articles,
+            loading: false
+        }
+} 
     render() {
         return (
             <div className="container my-3">
                <h2> Top Headlines </h2>
                 <div className="row">
                     <div className="col-md-4">
-                        <NewsItem title="myTitle" description="mydesc"/>
+                        <NewsItem title="myTitle" description="mydesc" url= "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+                        newsUrl="TODO"/>
                      </div>
-                <div className="col-md-4">
-                        <NewsItem title="myTitle" description="mydesc"/>
-                    </div>
-                <div className="col-md-4">
-                        <NewsItem title="myTitle" description="mydesc"/>
-                    </div>
+                     
                 </div>
             </div>
         )
--- src/componennets/NewsItem.js
+++ src/componennets/NewsItem.js
@@ -2,16 +2,17 @@ import { getByTitle } from '@testing-library/react'
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+   
     render() {
-       let {title, description} = this.props;
+       let {title, description, imageUrl, newsUrl} = this.props;
         return (
-            <div>
+            <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+    <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
             </div>
