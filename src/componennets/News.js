313e174f93141deaaccdf558db63c4329ae52d69
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 16:59:15 +0530

    changes in News.js and NewsItem.js

--- src/componennets/News.js
+++ src/componennets/News.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 export class News extends Component {
     render() {
         return (
             <div>
                 This is a news Component
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
             </div>
         )
     }
---
+++ src/componennets/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        return (
+            <div>
+                This is news Item
+            </div>
+        )
+    }
+}
+
+export default NewsItem
