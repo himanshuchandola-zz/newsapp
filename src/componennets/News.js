1946f4b93dcf52697664a00abcf827c51f3a21f3
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 16:53:38 +0530

    created news.js component

---
+++ src/componennets/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class News extends Component {
+    render() {
+        return (
+            <div>
+                This is a news Component
+            </div>
+        )
+    }
+}
+
+export default News
