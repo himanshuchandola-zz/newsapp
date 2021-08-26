941881530a2b6a5c0582dc2514299083653e445b
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 17:35:04 +0530

    Newsitems are placed under columns now

--- src/componennets/News.js
+++ src/componennets/News.js
@@ -4,14 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is a news Component
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
+            <div className="container my-3">
+               <h2> Top Headlines </h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="mydesc"/>
+                     </div>
+                <div className="col-md-4">
+                        <NewsItem title="myTitle" description="mydesc"/>
+                    </div>
+                <div className="col-md-4">
+                        <NewsItem title="myTitle" description="mydesc"/>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/componennets/NewsItem.js
+++ src/componennets/NewsItem.js
@@ -1,10 +1,19 @@
+import { getByTitle } from '@testing-library/react'
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
+       let {title, description} = this.props;
         return (
             <div>
-                This is news Item
+                <div className="card" style={{width: "18rem"}}>
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
             </div>
         )
     }
