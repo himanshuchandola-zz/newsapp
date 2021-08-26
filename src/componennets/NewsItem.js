b47b70c2fa7702715ef2fc85d57c9a909ef1af47
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 19:35:35 +0530

    News Url is opening on new page now

--- src/componennets/NewsItem.js
+++ src/componennets/NewsItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
    
     render() {
-       let {title, description,imageUrl} = this.props;
+       let {title, description, imageUrl, newsUrl} = this.props;
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
