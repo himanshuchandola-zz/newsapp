4a5b0dcb83ca8722842aebd164e0750fc16e9c08
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 20:34:07 +0530

    Did some changes in News title and Descriptions

--- src/componennets/NewsItem.js
+++ src/componennets/NewsItem.js
@@ -9,8 +9,8 @@ export class NewsItem extends Component {
                 <div className="card" style={{width: "18rem"}}>
                     <img src={!imageUrl?"https://s.france24.com/media/display/d1676b6c-0770-11e9-8595-005056a964fe/w:1280/p:16x9/news_1920x1080.png":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
