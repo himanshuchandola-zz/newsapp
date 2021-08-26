845cb74866cde3db3f0340110584164e2dc9bdd7
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 20:26:24 +0530

    null images issue solved it will be replaced by image that is specified

--- src/componennets/NewsItem.js
+++ src/componennets/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={!imageUrl?"https://s.france24.com/media/display/d1676b6c-0770-11e9-8595-005056a964fe/w:1280/p:16x9/news_1920x1080.png":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
