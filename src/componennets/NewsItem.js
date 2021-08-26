ce2c96a0f8de276dfdcec5827e9089267291d093
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 19:01:43 +0530

    removed megaurl and imageurl for time

--- src/componennets/NewsItem.js
+++ src/componennets/NewsItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
    
     render() {
-       let {title, description, imageUrl, newsUrl} = this.props;
+       let {title, description,} = this.props;
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
