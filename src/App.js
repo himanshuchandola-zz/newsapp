1dd6e242c900672d62be7ca8a2606fa7107930a8
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-26 16:54:33 +0530

    added news component in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import React, { Component } from 'react'
 import NavBar from './componennets/NavBar';
+import News from './componennets/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
+        <News/>
       </div>
     )
   }
